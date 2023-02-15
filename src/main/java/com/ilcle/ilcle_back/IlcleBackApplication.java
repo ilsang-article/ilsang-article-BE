bd1edf9ebfc4cde0e0c49bf10bec437d49e62668
ggggraceful <109974940+ggggraceful@users.noreply.github.com>
2023-02-15 13:38:10 +0900

    Add : files

---
+++ src/main/java/com/ilcle/ilcle_back/IlcleBackApplication.java
@@ -0,0 +1,13 @@
+package com.ilcle.ilcle_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlcleBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlcleBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ilcle/ilcle_back/IlcleBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilcle.ilcle_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlcleBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
