2aeb187bab8d57ff9a1e8f6cdb8f0fb8a406f884
soojin-dev <dvxz1125@gmail.com>
2023-02-16 13:34:07 +0900

    Add : 멤버 컨트롤러

--- src/main/java/com/ilcle/ilcle_back/controller/MemberController.java
+++ src/main/java/com/ilcle/ilcle_back/controller/MemberController.java
@@ -1,9 +1,14 @@
 package com.ilcle.ilcle_back.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "멤버 컨트롤러")
+@RequestMapping("/signup")
+@RestController
 @Controller
 public class MemberController {
-
 
 }
