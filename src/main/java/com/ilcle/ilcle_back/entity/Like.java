87073dbb6b28c57d72be64dae75382fd55f7af16
ggggraceful <109974940+ggggraceful@users.noreply.github.com>
2023-02-19 04:02:03 +0900

    Correct : Querydsl 설정(+ entity 오류)

--- src/main/java/com/ilcle/ilcle_back/entity/Like.java
+++ src/main/java/com/ilcle/ilcle_back/entity/Like.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.ui.context.Theme;
 
 import static jakarta.persistence.FetchType.LAZY;
 
--- src/main/java/com/ilcle/ilcle_back/entity/RefreshToken.java
+++ src/main/java/com/ilcle/ilcle_back/entity/RefreshToken.java
@@ -1,9 +1,6 @@
 package com.ilcle.ilcle_back.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
