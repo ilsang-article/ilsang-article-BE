a513410e9d5828b7c84cc8c7e2b05aba0a665c52
soojin-dev <dvxz1125@gmail.com>
2023-05-15 16:27:53 +0900

    CICD test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,28 +1,28 @@
-!/bin/bash
+#!/bin/bash
 BUILD_JAR=$(ls /home/ubuntu/action/build/libs/ilcle_back-0.0.1-SNAPSHOT.jar)
 JAR_NAME=$(basename $BUILD_JAR)
 
-echo "> 현재 시간: $(date)" >> /home/ubuntu/action/deploy.log
+echo "> 현재 시간: $(date)" >>/home/ubuntu/action/deploy.log
 
-echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/action/deploy.log
+echo "> build 파일명: $JAR_NAME" >>/home/ubuntu/action/deploy.log
 
-echo "> build 파일 복사" >> /home/ubuntu/action/deploy.log
+echo "> build 파일 복사" >>/home/ubuntu/action/deploy.log
 DEPLOY_PATH=/home/ubuntu/action/
 cp $BUILD_JAR $DEPLOY_PATH
 
-echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/action/deploy.log
+echo "> 현재 실행중인 애플리케이션 pid 확인" >>/home/ubuntu/action/deploy.log
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 
 if [ -z $CURRENT_PID ]
 then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/action/deploy.log
 else
-  echo "> kill -9 $CURRENT_PID" >> /home/ubuntu/action/deploy.log
+  echo "> kill -9 $CURRENT_PID" >>/home/ubuntu/action/deploy.log
   sudo kill -9 $CURRENT_PID
   sleep 5
 fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-source ~/.bash_profile
-echo "> DEPLOY_JAR 배포"    >> $DEPLOY_PATH/deploy.log
-sudo nohup java -jar $DEPLOY_JAR --spring.profiles.active=prod \ > $DEPLOY_PATH/nohup.out 2>&1 &
+source ~/.bashrc
+echo "> DEPLOY_JAR 배포" >>$DEPLOY_PATH/deploy.log
+nohup java -jar $DEPLOY_JAR >$DEPLOY_PATH/nohup.out 2>&1 &
